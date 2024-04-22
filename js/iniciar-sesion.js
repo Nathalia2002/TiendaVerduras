9a0a6b7a76298715cf9056f7d78cd2bdf35e35a0
Nathalia2002 <145623508+Nathalia2002@users.noreply.github.com>
2024-04-22 16:20:46 -0600

    conexion a la db
    conexion para registro e inicio de sesion

--- js/iniciar-sesion.js
+++ js/iniciar-sesion.js
@@ -1,89 +1,39 @@
-import { ManageAccount } from './firebaseconfig.js';
-
-document.getElementById("login-form").addEventListener("submit", (event) => {
-  event.preventDefault();
+// Inicializar Firebase, reemplaza esto con tu configuración
+const firebaseConfig = {
+    apiKey: "AIzaSyAkf7_W2fbdm6aSpgsl9k13rHOm4ZMkXHg",
+    authDomain: "pedidospuravida-1665d.firebaseapp.com",
+    projectId: "pedidospuravida-1665d",
+    storageBucket: "pedidospuravida-1665d.appspot.com",
+    messagingSenderId: "168211900848",
+    appId: "1:168211900848:web:94f1e23b1c9f66f404ce75"
+  };
+  firebase.initializeApp(firebaseConfig);
   
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-  const userType = document.getElementById("userType").value;
-
-  const account = new ManageAccount();
-  account.authenticate(email, password);
-  /* para controlar el cache*/
-  res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
-  alert("Registro exitoso. Serás redirigido a la página de inicio de sesión.");
-});
-console.log('Formulario de Inicio de Sesión');
-
-/*document.getElementById('login-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Evitar el comportamiento predeterminado del formulario
-
-    // Capturar los valores de los campos del formulario
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-
-    // Ejemplo de solicitud con fetch
-    fetch('url_del_servidor/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            email: email,
-            password: password
-        })
-    })
-    .then(response => {
-        if (response.ok) {
-            // Inicio de sesión exitoso
-            alert('¡Inicio de sesión exitoso!');
-            // Redirigir a la página principal u otra página
-            window.location.href = 'index.html';
-        } else {
-            // Error al iniciar sesión
-            alert('Credenciales incorrectas. Por favor, inténtelo de nuevo.');
-        }
-    })
-    .catch(error => {
-        console.error('Error al realizar la solicitud:', error);
-        alert('Hubo un error al iniciar sesión. Por favor, inténtelo de nuevo.');
-    });
-});
-
-
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
-
-// Agregar el listener al formulario de inicio de sesión
-document.getElementById('login-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Evitar el comportamiento predeterminado del formulario
-
-    // Capturar los valores de los campos del formulario
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-
-    // Verificar las credenciales con Firebase
-    const auth = getAuth();
-    signInWithEmailAndPassword(auth, email, password)
-       .then((userCredential) => {
-            // Inicio de sesión exitoso
-            const user = userCredential.user;
-            alert('¡Inicio de sesión exitoso!');
-            // Redirigir a la página principal u otra página
-            window.location.href = 'index.html';
-        })
-       .catch((error) => {
-            // Error al iniciar sesión
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            alert(`Error al iniciar sesión: ${errorMessage}`);
-        });
-});*/
+  // Obtener una referencia a la base de datos
+  var db = firebase.firestore();
+  
+  document.getElementById('login-form').addEventListener('submit', function(event) {
+      event.preventDefault();
+  
+      var email = document.getElementById('email').value;
+      var password = document.getElementById('password').value;
+  
+      // Buscar el usuario en Firestore
+      db.collection("users").where("email", "==", email).get()
+          .then((querySnapshot) => {
+              querySnapshot.forEach((doc) => {
+                  // doc.data() is never undefined for query doc snapshots
+                  var user = doc.data();
+                  if (user.password === password) {
+                      console.log("User logged in successfully!");
+                      window.location.href = "index.html";
+                  } else {
+                      console.log("Incorrect password.");
+                  }
+              });
+          })
+          .catch((error) => {
+              console.log("Error getting documents: ", error);
+          });
+  });
+  
--- js/signup.js
+++ js/signup.js
@@ -1,148 +1,37 @@
 
-import { ManageAccount } from './firebaseconfig.js';
+// Inicializar Firebase, reemplaza esto con tu configuración
+const firebaseConfig = {
+  apiKey: "AIzaSyAkf7_W2fbdm6aSpgsl9k13rHOm4ZMkXHg",
+  authDomain: "pedidospuravida-1665d.firebaseapp.com",
+  projectId: "pedidospuravida-1665d",
+  storageBucket: "pedidospuravida-1665d.appspot.com",
+  messagingSenderId: "168211900848",
+  appId: "1:168211900848:web:94f1e23b1c9f66f404ce75"
+};
+firebase.initializeApp(firebaseConfig);
 
-window.addEventListener('DOMContentLoaded', (event) => {
-    const formElement = document.getElementById("signup-form");
-    if(formElement) {
-        formElement.addEventListener("submit", (event) => {
-            event.preventDefault();
-            const name = document.getElementById("name").value;
-            const email = document.getElementById("email").value;
-            const password = document.getElementById("password").value;
-            const userType = document.getElementById("userType").value;
+// Obtener una referencia a la base de datos
+var db = firebase.firestore();
 
-            const account = new ManageAccount();
-            account.register(name,email,password,userType);
-
-             /* para controlar el cache*/
-              res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
-        });
-    }
-});
-
-console.log('Formulario de Registro');
-
-
-
-window.addEventListener('DOMContentLoaded', (event) => {
-  const createAccountLink = document.getElementById('create-account-link');
-  if(createAccountLink) {
-      createAccountLink.addEventListener('click', function(event) {
-          event.preventDefault();
-          window.location.href = 'crear-cuenta.html';
-      });
-  }
-
-  const forgotPasswordLink = document.getElementById('forgot-password-link');
-  if(forgotPasswordLink) {
-      forgotPasswordLink.addEventListener('click', function(event) {
-          event.preventDefault();
-          window.location.href = 'olvidar-contrasena.html';
-      });
-  }
-});
-
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-/*import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
-  
-  // signup.js
-  
-  document.getElementById('create-account-link').addEventListener('click', function(event) {
+document.getElementById('signup-form').addEventListener('submit', function(event) {
     event.preventDefault();
-    window.location.href = 'crear-cuenta.html';
-  });
-  
-  document.getElementById('forgot-password-link').addEventListener('click', function(event) {
-    event.preventDefault();
-    window.location.href = 'olvidar-contrasena.html';
-  });
-  
-  
-  document.getElementById('signup-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Evita el comportamiento predeterminado del formulario
-  
-    // Capturar los valores de los campos del formulario
-    var name = document.getElementById('name').value;
+
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
-  
-    // Ejemplo de solicitud con fetch
-    fetch('url_del_servidor/registro', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            name: name,
-            email: email,
-            password: password
-        })
+    var name = document.getElementById('name').value;
+
+    // Generar un ID único para el nuevo usuario
+    var userId = db.collection('users').doc().id;
+
+    db.collection("users").doc(userId).set({
+        name: name,
+        email: email,
+        password: password
     })
-    .then(response => {
-        if (response.ok) {
-            // La cuenta se creó exitosamente
-            alert('¡Cuenta creada exitosamente!');
-            // Redirigir a la página de inicio de sesión u otra página
-            window.location.href = 'login.html';
-        } else {
-            // Hubo un error al crear la cuenta
-            alert('Hubo un error al crear la cuenta. Por favor, inténtelo de nuevo.');
-        }
+    .then(() => {
+        console.log("Document successfully written!");
     })
-    .catch(error => {
-        console.error('Error al realizar la solicitud:', error);
-        alert('Hubo un error al crear la cuenta. Por favor, inténtelo de nuevo.');
+    .catch((error) => {
+        console.error("Error writing document: ", error);
     });
-  });
-  
-  
-  document.getElementById('forgot-password-link').onclick = (event) => {
-    event.preventDefault(); // Evita que el enlace redireccione
-    // Aquí puedes redirigir a la página de recuperación de contraseña
-    // Por ejemplo:
-    window.location.href = 'recuperar-contrasena.html';
-  };
- 
-  // Para diferenciar un administrador de un comprador 
-  function register() {
-    var userType = document.getElementById('userType').value;
-    if (userType === 'administrador') {
-        window.location.href = 'admin.html';
-    } else {
-        window.location.href = 'comprador.html';
-    }
-}
-
-// Función para agregar una persona a la base de datos
-function agregarPersona(usuario) {
-  // Obtener una referencia a la colección "personas" en la base de datos
-  const db = firebase.firestore();
-  const personasRef = db.collection('Usuarios');
-
-  // Agregar la persona a la colección "personas"
-  personasRef.add({
-    nombre: usuario.nombre,
-    contrasena : usuario.contrasena,
-    correo : usuario.correo,
-    tipo_usuario: usuario.tipo_usuario,
-    
-  })
- .then(() => {
-    // Mostrar un mensaje de éxito
-    console.log('Persona agregada exitosamente');
-  })
- .catch((error) => {
-    // Mostrar un mensaje de error
-    console.error('Error al agregar la persona:', error);
-  });
-}
-
-agregarPersona(persona);
-
-
-
-*/
-// Initialize Firebase
+});
