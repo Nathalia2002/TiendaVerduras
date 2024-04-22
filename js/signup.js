89882aa64b531ec946bf70a80ab03b4d79d426d0
Nathalia2002 <145623508+Nathalia2002@users.noreply.github.com>
2024-04-22 17:32:24 -0600

    correo ya existente

--- js/signup.js
+++ js/signup.js
@@ -20,20 +20,36 @@ document.getElementById('signup-form').addEventListener('submit', function(event
     var password = document.getElementById('password').value;
     var name = document.getElementById('name').value;
 
-    // Generar un ID único para el nuevo usuario
-    var userId = db.collection('users').doc().id;
-
-    db.collection("users").doc(userId).set({
-        name: name,
-        email: email,
-        password: password
-    })
-    .then(() => {
-        console.log("Document successfully written!");
-        // Mostrar mensaje de éxito
-        document.getElementById('signup-success').style.display = 'block';
-    })
-    .catch((error) => {
-        console.error("Error writing document: ", error);
-    });
-});
+     // Verificar si el correo electrónico ya está en uso
+     db.collection("users").where("email", "==", email).get()
+     .then((querySnapshot) => {
+         if (!querySnapshot.empty) {
+             // Si hay usuarios con el mismo correo electrónico, mostrar mensaje de error
+             console.error("El correo electrónico ya está en uso.");
+             // Mostrar mensaje de error al usuario
+             document.getElementById('signup-error').style.display = 'block';
+             return;
+         }
+ 
+         // Si el correo electrónico no está en uso, proceder con el registro
+         // Generar un ID único para el nuevo usuario
+         var userId = db.collection('users').doc().id;
+ 
+         db.collection("users").doc(userId).set({
+             name: name,
+             email: email,
+             password: password
+         })
+         .then(() => {
+             console.log("Document successfully written!");
+             // Mostrar mensaje de éxito
+             document.getElementById('signup-success').style.display = 'block';
+         })
+         .catch((error) => {
+             console.error("Error writing document: ", error);
+         });
+     })
+     .catch((error) => {
+         console.error("Error getting documents: ", error);
+     });
+ });
